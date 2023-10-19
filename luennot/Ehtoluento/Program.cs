8d1c8b8422402d548cbe270284c2938c9a9bc46c
Juho S <juho.salli@gmail.com>
2023-10-19 13:56:34 +0300

    lisää ehtoluentokoodit

---
+++ luennot/Ehtoluento/Program.cs
@@ -0,0 +1,35 @@
+﻿bool p = false;
+bool q = false;
+
+if (p || q)
+{
+    int a = 1 + 2;
+    Console.WriteLine(a * 2);
+}
+
+// int a = 9;
+// if (a > 10)
+// {
+//     Console.WriteLine("a on suurempi kuin 10");
+// }
+// else if (a == 10)
+// {
+//     Console.WriteLine("a on tasan 10");
+// }
+// else
+// {
+//     Console.WriteLine("a on pienempi kuin 10");
+// }
+
+
+
+// bool running = true;
+// if (running == true)
+// {
+//     Console.WriteLine("TOTTA");
+// }
+// else
+// {
+//     Console.WriteLine("EPÄTOTTA");
+// }
+// Console.WriteLine(1);
