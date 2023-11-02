3063fff72420d1e260404ad23febd876dbe25b9e
Juho S <juho.salli@gmail.com>
2023-11-02 13:53:23 +0200

    lisää arrayluentokoodit

---
+++ luennot/ArrayLuento/Program.cs
@@ -0,0 +1,25 @@
+﻿// string moro = "MORO";
+// string[] strings = { moro, "moi", "SAMK", "Pori" };
+// Console.WriteLine(strings[0]);
+
+// string moro = "MORO";
+// string[] strings = new string[] { moro, "moi", "SAMK", "Pori" };
+// Console.WriteLine(strings[0]);
+
+int[] ints = new int[100];
+ints[0] = 123;
+ints[10] = 90;
+int index = 12;
+ints[index] = 123423;
+Console.WriteLine(ints.Average());
+// int[] ints = new int[3];
+
+// ints[0] = 123;
+// ints[2] = 123;
+
+// Console.WriteLine(ints[0]);
+// Console.WriteLine(ints[1]);
+// Console.WriteLine(ints[2]);
+
+// bool[] bools = new bool[2];
+// Console.WriteLine(bools[0]);
