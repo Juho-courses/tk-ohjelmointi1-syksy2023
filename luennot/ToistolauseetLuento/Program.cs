7f16898d43c1104bf6f4a8d131af83650f75d396
Juho S <juho.salli@gmail.com>
2023-11-10 12:40:35 +0200

    toistolauseet luentokoodit

---
+++ luennot/ToistolauseetLuento/Program.cs
@@ -0,0 +1,30 @@
+﻿int[] ints = { 12, 123, 923923, 1231, 123 };
+
+int i = 0;
+while (i < ints.Length)
+{
+    Console.WriteLine(ints[i]);
+    i++;
+}
+
+// foreach (int luku in ints)
+// {
+//     if (luku == 123)
+//     {
+//         break;
+//     }
+//     Console.WriteLine(luku);
+// }
+
+// for (int i = 0; i <= ints.Length - 1; i++)
+// {
+//     // Console.WriteLine("i: " + i);
+//     Console.WriteLine(ints[i]);
+// }
+
+// for (alustus; ehto; muutos)
+// for (int i = 2; i < 10; i += 20)
+// {
+//     Console.WriteLine(i);
+// }
+// suoritus
